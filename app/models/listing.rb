b6b87128de6318e2f683b448511401b58a1c54b6
Iggy <igoririanto@rocketmail.com>
2016-12-01 13:35:29 -0700

    add alert

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,9 @@ class Listing < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 #  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
 
-#  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
 
 end
